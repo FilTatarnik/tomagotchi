210191c9bf5df8fce04eb1caeea9875934aaf551
Filip Tatarnik <ftatarnik@gmail.com>
2018-09-24 21:46:17 -0500

    tomagotchi pseudocode

---
+++ js/app.js
@@ -0,0 +1,26 @@
+console.log('words');
+
+//create an object for our tomagotchi
+//attach properties to the object
+//create a game object
+//add methods to the game object
+//instantiate the tomagotchi with jquery to the body, or whatever you want the game to take place
+//display the tomagotchi on the screen
+//display tomagotchi's stats; hunger(1-10), sleepiness(1-10), Boredom(1-10)
+//have age be a number that incements every... minute or so? maybe every 10 minutes?
+//make buttons
+//append them to the game screen
+//make the buttons clickable
+//btn 1 : pet
+//have this pet button have something to do with the hapiness level of your pet
+//--1 pet button press = .5 happiness or something like that
+//btn 2 : turn off lights
+//--have 1 turn off lights button press change the background of the game screen to black for 1 second and refill the pets sleepiness by x amount idk
+//btn 3 : play w pet
+//-- have 1 play w pet button press add 1+ boredom 
+//add a name property to your tomagotchi object, but also make it an empty variable so the player can input their own pet name 
+//every x minutes increase the pet's age x amount, until pet has turned.. idk 100 years old or something then it dies
+//..if pets hunger boredom or sleepiness hits 10 at any point, kill the pet
+//0-10 years, baby; 10-30 yrs, teenish; 30-90 adult
+//animate the tomagotchi somehow.. maybe every time one of the object properties goes up or down it moves one to the left or one to the right
+//game
