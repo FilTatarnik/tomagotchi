2a1eff01bc44b0573bab0f0b4b0df9549095fb04
Filip Tatarnik <ftatarnik@gmail.com>
2018-09-26 11:17:11 -0500

    created game object. User can create a katagachi with a name

--- js/app.js
+++ js/app.js
@@ -44,59 +44,129 @@ PSEUDOCODE
 
 
 
+/************************************************
+Below is the function that is attached to the pet button that will increase the Happiness meter by one per click... i guess. also make it so hapiness starts at like.. 20 or something at decreases one every 3000-4000 milliseconds or soemthing
+*************************************************/
+
+/************************************************
+Below is the game object. The game object will have all of the methods that are associated with
+the game itsself; example: startGame().....uhh and other methods.... figure this out asap
+
+*************************************************/
+// //game object <------- no idea what to do w that game object
 
 /************************************************
 Below are global variables
 *************************************************/
-	let time = 0;
-	let hunger = 0;
-	let sleepiness = 0;
-	let boredom = 0;
+	// let time = 0;
+	// let hunger = 0;
+	// let sleepiness = 0;
+	// let boredom = 0;
 /******************************************
 Our pet object. Our pet has different constructors.. i'm not too sure what this._____ is or does. read up on it
 *******************************************/
-class Katagachi{
-  	constructor(name, age, sleepiness, boredom, hunger ) {
+class Katagachi {
+  	constructor(name) {
 	    this.name = name;
-	    this.age = age;
-	    this.boredom = boredom;		       /* <--no idea what this stuff is*/
-	    this.sleepiness = sleepiness;
-	    this.hunger = hunger;
+	    this.age = 0;
+	    this.boredom = 0;		       /* <--no idea what this stuff is*/
+	    this.sleepiness = 0;
+	    this.hunger = 0;
 	}
-/************************************************
-Below are the methods associated with the katagachi
+	/************************************************
+	Below are the methods associated with the katagachi
 
-*************************************************/
-
-
+	*************************************************/
 	isAlive() {
-		if(this.boredom ,
-		   this.sleepiness,
-		   this.hunger > 0){return true;}
-		   		 else if (this.boredom,
-		   				  this.sleepiness,
-		   				  this.hunger = 0){return false;}
-		   		}
 
+		if(this.boredom < 10 && this.sleepiness < 10 && this.hunger < 10){
+			return true;
 		}
+		else {
+		   	return false;
+		}
+	}
+
+		
 		//if boredom, sleepiness, and hunger are > 0 --> isAlive true
 		//else if boredom, sleepiness, and hunger are = 0 --->isAlive false
 	
 
-	// feedUp() {
-	// 	// subtract 1 from hunger
-	// }
+	feedUp() {
+		// subtract 1 from hunger
+		hunger--;
+	}
 
 	// lightsOff()  {
 
 	// 	//subtract 1 from sleepiness
 	// }
 
-	// petUp() {
+	petDown() {
+		// if($('#petPetBut').on('click', ()=> {
+		// 	this.petPetBut--;
+		// }))
 	// 	//subtract 1 from boredom
-	// }
+	}
+
+}
 
 
+const game = {
+
+	katagachi: null, //initial value
+
+	setNameAndStartGame() {
+
+		// get the name
+		const name = $('#nameInput').val()
+		console.log(name, " this is a name");
+
+		// instantiate, pass in name (get from input with jQuery)
+		const fil = new Katagachi(name);
+
+		// store it in the game object so it won't go away when function is finished
+		this.katagachi = fil;
+
+
+		// remove the input field
+		
+		// remove button
+
+		// display just the name where those were
+
+		// timer start
+
+
+
+
+	},
+
+	printStats() {
+
+	}
+
+}
+
+
+const shoes = {
+    price: 50,
+ 	increasePrice () {
+ 		this.price = 1000;
+ 	}
+}   
+
+
+class Person {
+	constructor(name) {
+		this.name = name;
+	}
+	greet() {
+		console.log("Hello I am " + this.name)
+	}
+}
+
+const filip = new Person('fil');
 
 /************************************************
 Below is the function that is attached to the name button that starts the timer
@@ -110,11 +180,47 @@ const setTimer = () => {
 			clearInterval(interval);
 
 		}
-	 	$('#timer').text('Timer ' + time + 's');
+	 	$('#timer').text('Timer ' + time + ' hour');
 	}, 1000)
 
 };
 
+const boredTimer = () =>{
+		const interval = setInterval(() => {
+		boredom++;
+		if(boredom === 12){
+			clearInterval(interval);
+
+		}
+	 	$('#boredom').text('Boredom ' + boredom );
+	}, 5000)
+};
+
+const sleepTimer = () =>{
+		const interval = setInterval(() => {
+		sleepiness++;
+		if(sleepiness === 12){
+			clearInterval(interval);
+			// gameOver(); <----make game over function that lets the user know when the game has ended
+
+
+		}
+	 	$('#sleepiness').text('Sleepiness ' + sleepiness );
+	}, 8000)
+};
+
+const hungerTimer = () =>{
+		const interval = setInterval(() => {
+		hunger++;
+		if(hunger === 12){
+			clearInterval(interval);
+			// gameOver(); <----make game over function that lets the user know when the game has ended
+			
+
+		}
+	 	$('#hunger').text('hunger ' + hunger );
+	}, 3000)
+};
 /************************************************
 Below is the where i'm going to build the timer object. and stick the timer variable inside of it, and i will handle all the button setInterval timers in there with if statements i think...
 
@@ -124,17 +230,8 @@ Below is the where i'm going to build the timer object. and stick the timer vari
 // }
 
 /************************************************
-Below is the function that is attached to the pet button that will increase the Happiness meter by one per click... i guess. also make it so hapiness starts at like.. 20 or something at decreases one every 3000-4000 milliseconds or soemthing
-*************************************************/
-
 /************************************************
-Below is the game object. The game object will have all of the methods that are associated with
-the game itsself; example: startGame().....uhh and other methods.... figure this out asap
-
-*************************************************/
-// //game object <------- no idea what to do w that game object
-//  
-/************************************************
+EVENT LISTENERS---
 Below are Listeners that are attached to the buttons on screen. 
 each listener will have funcitons that do different things to the game, like
 pet button will call the petUp function that decreases the pets sadness by one per click
@@ -146,13 +243,22 @@ $('button').on('click', ()=>{
 	console.log("buttons work");
 })
 
-$('#namePetBut').on('click', ()=>{
-	console.log('name pet button works');
-	setTimer();
+$('#namePetBut').on('click', () => {
+
+	game.setNameAndStartGame();
+
+	// console.log('name pet button works');
+	// setTimer();
+	// boredTimer();
+	// sleepTimer();
+	// hungerTimer();
+	
 })
 
 $('#petPetBut').on('click', ()=>{
 	console.log('pet button works');
+	petDown();
+
 })
 
 $('#lightOffBut').on('click', ()=>{
@@ -163,9 +269,16 @@ $('#feedBut').on('click', ()=>{
 	console.log('feed button works');
 })
 
-
-
-
+/********************************************************************
+Event listener for removing input field after Create Katagachi is hit // doesn't do anything atm
+********************************************************************/
+// $('#nameInput').on('click', () => {
+// 	if ($('#nameInput').val() == "Katagachi!")
+// 		$('#nameInput').val("")
+// });
+/********************************************************************
+When user presses 
+********************************************************************/
 
 
 
